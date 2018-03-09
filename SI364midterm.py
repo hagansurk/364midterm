aac95d5a8b9dd1edb0357b37d37da86e0c47dfac
Hagan <hhsurk@umich.edu>
2018-03-09 11:43:02 -0500

    Completed most of the checklist and more, just need to work on the query for all Users page.

--- SI364midterm.py
+++ SI364midterm.py
@@ -47,7 +47,6 @@ def create_food(food, diet, allergy_health, user_id):
         db.session.add(food_enter)
         db.session.commit()
         find_recipe(food, diet, allergy_health, user_id)
-    
     else:
         return food1
 
@@ -63,7 +62,6 @@ def find_recipe(food, diet, allergy_health, user_id):
             health_lab = str(dic['recipe']['healthLabels'])
             ingr = str(dic['recipe']['ingredientLines'])
             cals = dic['recipe']['calories']
-        print(recipe_name,ingr,cals)
         rec = Recipes.query.filter_by(recipe = recipe_name).first()
         if rec is None:
             rec_enter = Recipes(recipe=recipe_name,ingredients=ingr,health_labels=health_lab, cals=cals, user_id=user_id, food=food_id)
@@ -71,8 +69,7 @@ def find_recipe(food, diet, allergy_health, user_id):
             db.session.commit()
         else:
             return rec
-#curl "https://api.edamam.com/search?q=chicken&app_id=${c32dd9d7}&app_key=${3a1dc96c14fb8d3a339815d072d89fbb}&from=0&to=3&calories=gte%20591,%20lte%20722&health=alcohol-free"
-    
+
 ##################
 ##### MODELS #####
 ##################
@@ -86,7 +83,7 @@ class Food(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     recipe = db.relationship('Recipes', backref='Food')
     def __repr__(self):
-        return "{} (ID: {})".format(self.food_name, self.id, )
+        return "{} (ID: {})".format(self.food_name, self.id)
 
 class Recipes(db.Model):
     __tablename__ = "recipes"
@@ -106,19 +103,19 @@ class User(db.Model):
     user_name = db.Column(db.String(64))
     first_name = db.Column(db.String(64))
     last_name = db.Column(db.String(64))
-    fav_recipe = db.relationship('Favorites', backref='User')
+    #fav_recipe = db.relationship('Favorites', backref='User')
     called_recipes = db.relationship('Recipes', backref='User')
     food_entered = db.relationship('Food',backref='User')
     def __repr__(self):
         return "{} (ID: {})".format(self.user_name,self.id)
 
-class Favorites(db.Model):
-    __tablename__ = "favorite recipes"
-    id = db.Column(db.Integer, primary_key=True)
-    recipe = db.Column(db.String(500))
-    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
-    def __repr__(self):
-        return "{} | ID:{}".format(self.recipe,self.user_id)
+# class Favorites(db.Model):
+#     __tablename__ = "favorite recipes"
+#     id = db.Column(db.Integer, primary_key=True)
+#     recipe = db.Column(db.String(500))
+#     user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+#     def __repr__(self):
+#         return "{} | ID:{}".format(self.recipe,self.user_id)
 
 ###################
 ###### FORMS ######
@@ -137,59 +134,63 @@ class NameForm(FlaskForm):
     last = StringField("Enter your Last Name: ", validators=[Required()])
     submit = SubmitField('Submit')
 
-class FavoriteForm(FlaskForm):
-    username = StringField("Enter your Username here: ", validators=[Required(),one_word])
-    fav = SelectMultipleField("Choose Favorite Recipes: ", choices = [], option_widget = widgets.CheckboxInput(), widget=widgets.ListWidget(prefix_label=False)) #this code I found on http://www.ergo.io/tutorials/persuading-wtforms/persuading-wtforms-to-generate-checkboxes/
-    submit = SubmitField('Submit')
+# class FavoriteForm(FlaskForm):
+#     username = StringField("Enter your Username here: ", validators=[Required(),one_word])
+#     fav = SelectMultipleField("Choose Favorite Recipes: ", choices = ['Hey','hello','whats up'], option_widget = widgets.CheckboxInput(), widget=widgets.ListWidget(prefix_label=False)) #this code I found on http://www.ergo.io/tutorials/persuading-wtforms/persuading-wtforms-to-generate-checkboxes/
+#     submit = SubmitField('Submit')
 
 #######################
 ###### VIEW FXNS ######
 #######################
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
-@app.route('/', methods=['GET','POST'])
+
+@app.route('/')
 def home():
-    form = NameForm() # User should be able to enter name after name and each one will be saved, even if it's a duplicate! Sends data with 
-    user_n,firs_n,last_n = None,None,None
-    if request.method == 'POST' and form.validate_on_submit():
-        user_n = form.username.data
-        first_n = form.first.data
-        last_n = form.last.data
-        #print(user_n,first_n,last_n)
-        user = User.query.filter_by(user_name = user_n).first()
+    form = NameForm() 
+    return render_template('name.html',form=form)
+
+@app.route('/home1',methods=['GET','POST'])
+def answers():
+    form = NameForm()
+    if request.args:
+        user_name = request.args.get('username')
+        first = request.args.get('first')
+        last = request.args.get('last')
+        print(user_name,first,last)
+        
+        user = User.query.filter_by(user_name = user_name).first()
         if user is None:
-            username = User(user_name = user_n,first_name = first_n,last_name = last_n)
+            username = User(user_name = user_name,first_name = first,last_name = last)
             db.session.add(username)
             db.session.commit()
-            flash('User Successfully Created')
+            flash('User successfully created')
             return redirect(url_for('enter_food'))
         else:
-            user = User.query.filter_by(user_name = user_n).first()
+            user = User.query.filter_by(user_name = user_name).first()
             print(user)
             flash('Username is taken, please enter a new name')
             return redirect(url_for('home'))
-        
-    else:
-        print('i wont work')
-    errors = [x for x in form.errors.values()]
-    if len(errors) > 0:
-        flash('Error in the submission of the form - '+str(errors))
-    return render_template('name.html',form=form)
+
+    
 
 @app.route('/food_entry', methods=['POST','GET'])
 def enter_food():
     form = FoodForm() # User should be able to enter name after name and each one will be saved, even if it's a duplicate! Sends data with GET
+    # name_entries = request.args.get()
+    # print(name_entries)
     if request.method == 'POST' and form.validate_on_submit():
         name = form.user_name.data
         food = form.food_name.data.lower()
         diet = form.diet.data
         allergy_health = form.health.data
-        print(food,diet,allergy_health)
         username1 = User.query.filter_by(user_name=name).first()
         user_id = username1.id
         create_food(food,diet,allergy_health,user_id)
         food1 = Food.query.filter_by(food_name=food,diet=diet,health=allergy_health).first()
         if food1:
-            print(food1)
             flash('Food Added Successfully')
             return redirect(url_for('see_all_recipes'))
 
@@ -206,7 +207,6 @@ def enter_food():
 def see_all_recipes():
     all_recipes=[]
     rec = Recipes.query.all()
-    print(rec)
     for elem in rec:
         rec_name = elem.recipe
         ingr = elem.ingredients
@@ -220,7 +220,22 @@ def see_all_recipes():
 
     return render_template('all_recipes.html', all_recipes=all_recipes)
 
-
+@app.route('/all_users')
+def see_users():
+    all_users = []
+    users = User.query.all()
+    for elem in users:
+        name = elem.user_name
+        user_id = elem.id
+        foods = Food.query.filter_by(user_id = user_id).all()
+        recipes = Recipes.query.filter_by(user_id=user_id).all()
+        for elem1 in foods:
+            food_name = elem1.food_name
+            for elem2 in recipes:
+                recipe_name = elem2.recipe
+                tupl = (name,food_name,recipe_name)
+                all_users.append(tupl)
+    return render_template('all_users.html', all_users=all_users)
 
 
 ## Code to run the application...
